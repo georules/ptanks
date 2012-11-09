8adfd9d5dc2d0345259a37990df94275ce4e3633
Geo Miller <geoffery.miller@gmail.com>
2012-11-09 10:19:37 -0500

    first

---
+++ src/Grid2D.java
@@ -0,0 +1,14 @@
+
+public class Grid2D {
+	int x, y;
+	public Grid2D() {
+		this.x = this.y = 10;
+	}
+	public Grid2D(int l) {
+		this.x = this.y = l;
+	}
+	public Grid2D(int x, int y) {
+		this.x = x; this.y = y;
+	}
+	
+}
