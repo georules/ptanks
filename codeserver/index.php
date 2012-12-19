293448cfb009d2fce28fe79a4c70014e9cadb4a7
Geo Miller <geoffery.miller@gmail.com>
2012-12-18 19:49:23 -0600

    server coding

--- codeserver/index.php
+++ codeserver/index.php
@@ -1,21 +1,32 @@
 <?php
+require_once('./config.php');
 include_once './epiphany/src/Epi.php';
 Epi::setPath('base', './epiphany/src');
-Epi::init('api','database');
+Epi::init('api','database','debug');
+Epi::setSetting('debug',true);
 
-EpiDatabase::employ('mysql','ptanks'
+EpiDatabase::employ(DBTYPE,DBNAME,DBHOST,DBUSER,DBPASS);
 
-getRoute()->get('/', 'showEndpoints');
+getRoute()->get('/','show');
+getRoute()->get('/(\d+)', 'show');
 getRoute()->post('/code','code');
 getRoute()->run();
 
-function showEndpoints()
-{
-	echo "hi";
+function show($id = null)	{
+	if (empty($id))	{
+		$data = getDatabase()->all("SELECT * from code");
+		var_dump($data);
+	}
+	else	{
+		$code = getDatabase()->one("SELECT code FROM code WHERE id = :id", array('id'=>$id));
+		$code = $code['code'];
+		echo($code);
+	}
 }
 
 function code()	{
-
+	$code = $_POST['code'];
+	getDatabase()->execute("INSERT INTO code (code) VALUES (:code)",array('code'=>$code));
 }
 
 ?>
